05d15f6b32cc570820b5bdf99af3531bb7e39399
girasebhushan <137667897+girasebhushan@users.noreply.github.com>
2023-08-06 09:49:19 +0530

    Add files via upload

---
+++ program361.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class StringX
+{
+    public String RemoveWhiteSpace(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        char Brr[] = new char[Arr.length];
+
+        int i = 0;
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if(Arr[iCnt] != ' ')
+            {
+                Brr[i] = Arr[iCnt];
+                i++;
+            }
+        }
+        return new String(Brr);
+    }
+}
+
+class program360
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        StringX obj = new StringX();
+        String sret = obj.RemoveWhiteSpace(name);
+
+        System.out.println("Result is : "+sret);
+    }
+}import java.util.*;
+
+class StringX
+{
+    public String RemoveWhiteSpace(String str)
+    {
+        char Arr[] = str.toCharArray();
+        String output = "";
+
+        for(int iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if(Arr[iCnt] != ' ')
+            {
+                output = output + Arr[iCnt];
+            }
+        }
+        return output;
+    }
+}
+
+class program361
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        StringX obj = new StringX();
+        String sret = obj.RemoveWhiteSpace(name);
+
+        System.out.println("Result is : "+sret);
+    }
+}
